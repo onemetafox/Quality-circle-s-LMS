a0674e51bdf0a691ec96305e67929a0dbd53e43c
James Coulter <jamesdcoulter1972@gmail.com>
2021-10-18 03:55:28 -0800

    update invoice detail page

--- application/views/_templates/company_invoice.php
+++ application/views/_templates/company_invoice.php
@@ -250,16 +250,31 @@
           </tr>
         </tbody>
         <tfoot>
-          <!-- <tr>
-            <td colspan="2"></td>
-            <td colspan="2">SUBTOTAL</td>
-            <td>$<?= (100 - $tax->value)/100 * $invoice->amount?></td>
+          <tr>
+            <td colspan="3"></td>
+            <td colspan="2">PRICE</td>
+            <td>$<?= $invoice->price?></td>
           </tr>
           <tr>
-            <td colspan="2"></td>
-            <td colspan="2">TAX <?= $tax->value?>%</td>
-            <td>$<?= ($tax->value/100) * $invoice->amount?></td>
-          </tr> -->
+            <td colspan="3"></td>
+            <td colspan="2">DISCOUNT(%)</td>
+            <td><?= $invoice->discount?>(%)</td>
+          </tr>
+          <tr>
+            <td colspan="3"></td>
+            <td colspan="2">SUBTOTAL</td>
+            <td>-$<?= $invoice->price * ($invoice->discount/100)?></td>
+          </tr>
+          <tr>
+            <td colspan="3"></td>
+            <td colspan="2">TAX TYPE </td>
+            <td><?= $invoice->tax_type=='0'? 'Percentage':'Fixed'?></td>
+          </tr>
+          <tr>
+            <td colspan="3"></td>
+            <td colspan="2">TAX</td>
+            <td><?= $invoice->tax_type=='1'? '$':''?><?=$invoice->tax_rate?><?= $invoice->tax_type=='0'? '(%)':''?></td>
+          </tr>
           <tr>
             <td colspan="3"></td>
             <td colspan="2">GRAND TOTAL</td>
@@ -268,10 +283,10 @@
         </tfoot>
       </table>
       <div id="thanks">Thank you!</div>
-      <div id="notices">
+      <!-- <div id="notices">
         <div>NOTICE:</div>
         <div class="notice">A finance charge of 1.5% will be made on unpaid balances after 30 days.</div>
-      </div>
+      </div> -->
     </main>
     <footer>
       Invoice was created on a computer and is valid without the signature and seal.
